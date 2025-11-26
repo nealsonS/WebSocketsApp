691f327452ec9167aa84a716c5107d3ce039a36c
Nealson <nealsonsetiawan03@gmail.com>
2025-11-26 12:07:16 -0800

    websocket connection

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
+const io = Server(server);
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
+  res.sendFile(join(__dirname, "index.html"));
 });
 
+io.on("connection", (socket) => {
+  console.log("someone connected!");
+});
 server.listen(3000, () => {
   console.log("server running at http://localhost:3000");
 });
